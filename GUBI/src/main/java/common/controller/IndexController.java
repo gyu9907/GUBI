24cd70f5828253fce99362e3178f6188df0bb121
gyu9907 <gyu9907@gmail.com>
2024-12-13 12:35:03 +0900

    init: 초기 구성

---
+++ GUBI/src/main/java/common/controller/IndexController.java
@@ -0,0 +1,15 @@
+package common.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class IndexController extends AbstractController {
+	
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		super.setRedirect(false);
+		super.setViewPage("/WEB-INF/index.jsp");
+	}
+
+}
---
+++ GUBI/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>GUBI is a design company dedicated to celebrating the luxury of living life.</title>
+</head>
+<body>
+메인 페이지
+</body>
+</html>
